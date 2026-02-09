c87421bdf359c161b6ad7d4d6642ac26773a4ef0
Oplexitie <arthur.cottreau@gmail.com>
2026-02-10 00:34:22 +0100

    Clean up test scripts and scenes

--- Scripts/Examples/save_test.gd
+++ Scripts/Examples/save_test.gd
@@ -1,4 +1,4 @@
-extends VBoxContainer
+extends Node
 
 @export var is_saving: bool
 @export var is_loading: bool
--- Scripts/Examples/sound_test.gd
+++ Scripts/Examples/sound_test.gd
@@ -1,37 +1,34 @@
 extends Control
 
-@export var audio_test: AudioStreamWAV
-@export var audio_test2: AudioStreamOggVorbis
+@export var sfx_list: Dictionary[String,AudioStreamWAV]
+@export var bgm_list: Dictionary[String,AudioStreamOggVorbis]
 
-func _on_channel_sfx() -> void:
-	AudioManager2.play_sfx(audio_test,0)
+func _on_channel_sfx(sfx_key: String, override: bool = true) -> void:
+	AudioChannelManager.play_sfx(sfx_list[sfx_key],0, 0.0, override)
 
-func _on_channel_sfx_cut() -> void:
-	AudioManager2.play_sfx(audio_test,0,0.0, false)
-
-func _on_channel_track() -> void:
-	AudioManager2.play_music(audio_test2)
-
-func _on_pool_sfx() -> void:
-	AudioManager.play_sfx(audio_test)
-
-func _on_pool_track() -> void:
-	AudioManager.play_music(audio_test2)
+func _on_channel_bgm(bgm_key: String) -> void:
+	AudioChannelManager.play_music(bgm_list[bgm_key])
 
 func _on_fade_out_channel() -> void:
-	AudioManager2.fade_music(-60,2)
-	await AudioManager2.audio_music.tween.finished
-	AudioManager2.stop_music()
+	AudioChannelManager.fade_music(-60,2)
+	await AudioChannelManager.audio_music.tween.finished
+	AudioChannelManager.stop_music()
+
+func _on_pool_sfx(sfx_key: String) -> void:
+	AudioPoolManager.play_sfx(sfx_list[sfx_key])
+
+func _on_pool_bgm(bgm_key: String) -> void:
+	AudioPoolManager.play_music(bgm_list[bgm_key])
 
 func _on_fade_out_pool() -> void:
-	AudioManager.fade_music(-60,2)
-	await AudioManager.audio_music.tween.finished
-	AudioManager.stop_music()
+	AudioPoolManager.fade_music(-60,2)
+	await AudioPoolManager.audio_music.tween.finished
+	AudioPoolManager.stop_music()
 
 func _stop_sfx() -> void:
-	AudioManager.stop_sfx()
-	AudioManager2.stop_sfx()
+	AudioPoolManager.stop_sfx()
+	AudioChannelManager.stop_sfx()
 
 func _stop_track() -> void:
-	AudioManager.stop_music()
-	AudioManager2.stop_music()
+	AudioPoolManager.stop_music()
+	AudioChannelManager.stop_music()
